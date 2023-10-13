f39f8407443b5f95abfba584dca3a0712a877902
Gildemberg Leite <gildembergleite@gmail.com>
2023-10-13 18:17:15 -0300

    feat: list posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
 import AsideProfile from '@/components/AsideProfile'
 import Header from '@/components/Header'
+import Post from '@/components/Post'
+import data from '../../data.json'
 
 export default function Home() {
   return (
     <>
       <Header />
-      <section className="flex w-full p-8">
-        <div className="flex w-full max-w-7xl justify-center items-center">
+      <div className="flex w-full justify-center p-8">
+        <div className="flex w-full max-w-7xl justify-center items-start gap-8">
           <AsideProfile />
+          <section className="flex flex-1 flex-col gap-8">
+            {data.posts.map((post, index) => (
+              <Post
+                key={index}
+                userId={post.userId}
+                content={post.content}
+                hashtags={post.hashtags}
+                comments={post.comments}
+              />
+            ))}
+          </section>
         </div>
-      </section>
+      </div>
     </>
   )
 }
